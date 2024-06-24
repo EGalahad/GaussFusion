d0e69b78e82b87b786e571130a59ec0ba03e8a16
jasmine27-alt <jaswong548@gmail.com>
2024-06-24 22:18:59 +0800

    Update preprocess.py around wall
    Former-commit-id: e6f50df3b72587618a381ccab28ca43d19a8bf46
    Former-commit-id: e2c457b5f1acb9408a3f75e8616cefefcb347624 [formerly 57ff1b86e602b432918370d216635c4198f32b93]
    Former-commit-id: 5b26cd13c75d8bf10851075a8f9ca998f7cd2a52

--- data_processing/preprocess.py
+++ data_processing/preprocess.py
@@ -2,7 +2,7 @@ from utils import *
 import os
 
 def generate_background():
-    file_path = 'data/parking_spots_in_garage.glb'
+    file_path = 'data/neon_bedroom.glb'
     scene_name = "background"
     output_dir = f'{scene_name}/images'
     output_json = f'{scene_name}/output/cameras.json'
@@ -14,6 +14,11 @@ def generate_background():
     mesh = merge_geometries(scene)
     if not mesh:
         return
+    
+   # Rotate the mesh 90 degrees around the axis [1, 0, 1]
+    #axis = np.array([1, 0, 1])
+    #angle = np.pi / 2  # 90 degrees in radians
+    #mesh = apply_rotation(mesh, axis, angle)
 
     pv_mesh = convert_to_pyvista(mesh)
     if not pv_mesh:
@@ -26,12 +31,17 @@ def generate_background():
     intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
     num_views = 125
-    camera_positions = generate_interior_camera_positions(mesh, num_views)
+    radius = 5
+    height = 3  # Height of the room
+    wall_distance = 10  # Distance of the camera from the center along the wall
 
-    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, show_scalar_bar=False)
+    camera_positions, focal_points = generate_camera_positions_along_wall(num_views, radius, height, wall_distance)
+
+    camera_data = render_images_bg(pv_mesh, texture, camera_positions, focal_points, intrinsics, output_dir)
 
     os.makedirs(os.path.dirname(output_json), exist_ok=True)
     save_camera_data(camera_data, output_json)
+
 
 def generate_object():
     file_path = 'data/ficus_bonsai.glb'
@@ -42,10 +52,8 @@ def generate_object():
     scene = load_model(file_path)
     if not scene:
         return
-    
+
     mesh = merge_geometries(scene)
-    if not mesh:
-        return
 
     pv_mesh = convert_to_pyvista(mesh)
     if not pv_mesh:
@@ -58,10 +66,10 @@ def generate_object():
     intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
     num_views = 125
-    radius = 2
-    camera_positions = generate_camera_positions(num_views, radius)  # Smaller radius for object
+    radius = 10
+    camera_positions = generate_camera_positions(num_views, radius)
 
-    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, show_scalar_bar=False)
+    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
 
     os.makedirs(os.path.dirname(output_json), exist_ok=True)
     save_camera_data(camera_data, output_json)
