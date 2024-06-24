6a05fe136b7e6c313aa1f3c3b11920959f05279d
jasmine27-alt <jaswong548@gmail.com>
2024-06-24 16:23:47 +0800

    Update preprocess.py with parking lot
    Former-commit-id: 505e055f785b6351ad3336f11a8be5b9d6aa43d2
    Former-commit-id: f0d923c1f41d8241ec53b5c294c8409f1f60d939 [formerly cffad6cc6a48089e31ad4e096700259b33bbb101]
    Former-commit-id: 1de960b670551fdd587f40cbe7c007fff2745482

--- data_processing/preprocess.py
+++ data_processing/preprocess.py
@@ -2,7 +2,7 @@ from utils import *
 import os
 
 def generate_background():
-    file_path = 'data/vr_gallery.glb'
+    file_path = 'data/parking_spots_in_garage.glb'
     scene_name = "background"
     output_dir = f'{scene_name}/images'
     output_json = f'{scene_name}/output/cameras.json'
@@ -26,14 +26,12 @@ def generate_background():
     intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
     num_views = 125
-    radius = 10
-    camera_positions = generate_camera_positions(num_views, radius)
+    camera_positions = generate_interior_camera_positions(mesh, num_views)
 
-    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
+    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, show_scalar_bar=False)
 
     os.makedirs(os.path.dirname(output_json), exist_ok=True)
     save_camera_data(camera_data, output_json)
-
 
 def generate_object():
     file_path = 'data/ficus_bonsai.glb'
@@ -46,12 +44,14 @@ def generate_object():
         return
     
     mesh = merge_geometries(scene)
+    if not mesh:
+        return
 
     pv_mesh = convert_to_pyvista(mesh)
     if not pv_mesh:
         return
 
-    texture = handle_texture(scene, pv_mesh)
+    texture = handle_texture(mesh, pv_mesh)
 
     fx, fy = 800, 800
     cx, cy = 640, 480
@@ -59,9 +59,9 @@ def generate_object():
 
     num_views = 125
     radius = 2
-    camera_positions = generate_camera_positions(num_views, radius)
+    camera_positions = generate_camera_positions(num_views, radius)  # Smaller radius for object
 
-    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
+    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, show_scalar_bar=False)
 
     os.makedirs(os.path.dirname(output_json), exist_ok=True)
     save_camera_data(camera_data, output_json)
