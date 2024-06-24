e170da2ebfdc24ee3055dc63c78b4b8e176dab3d
jasmine27-alt <jaswong548@gmail.com>
2024-06-24 22:21:21 +0800

    Update preprocess.py: Center Camera
    Former-commit-id: 8896fe7ea305422064ecd242952442cfa6e49fbb
    Former-commit-id: ae3c1d5edaa6dfeaad9e9fab5edb6fb3b1c21d97 [formerly 93b72ac8cef03ac6922358daf3a70313c8342b01]
    Former-commit-id: 90e1f2e21e4b116fbd57ffc16f3d6f77f3c1a9c7

--- data_processing/preprocess.py
+++ data_processing/preprocess.py
@@ -1,79 +1,95 @@
 from utils import *
 import os
 
+
 def generate_background():
-    file_path = 'data/neon_bedroom.glb'
-    scene_name = "background"
-    output_dir = f'{scene_name}/images'
-    output_json = f'{scene_name}/output/cameras.json'
+   file_path = 'data/cozy_modern_bedroom.glb'
+   scene_name = "background"
+   output_dir = f'{scene_name}/images'
+   output_json = f'{scene_name}/output/cameras.json'
 
-    scene = load_model(file_path)
-    if not scene:
-        return
 
-    mesh = merge_geometries(scene)
-    if not mesh:
-        return
-    
+   scene = load_model(file_path)
+   if not scene:
+       return
+
+
+   mesh = merge_geometries(scene)
+   if not mesh:
+       return
+
+
    # Rotate the mesh 90 degrees around the axis [1, 0, 1]
-    #axis = np.array([1, 0, 1])
-    #angle = np.pi / 2  # 90 degrees in radians
-    #mesh = apply_rotation(mesh, axis, angle)
+   #axis = np.array([1, 0, 1])
+   #angle = np.pi / 2  # 90 degrees in radians
+   #mesh = apply_rotation(mesh, axis, angle)
 
-    pv_mesh = convert_to_pyvista(mesh)
-    if not pv_mesh:
-        return
 
-    texture = handle_texture(mesh, pv_mesh)
+   pv_mesh = convert_to_pyvista(mesh)
+   if not pv_mesh:
+       return
 
-    fx, fy = 800, 800
-    cx, cy = 640, 480
-    intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
-    num_views = 125
-    radius = 5
-    height = 3  # Height of the room
-    wall_distance = 10  # Distance of the camera from the center along the wall
+   texture = handle_texture(mesh, pv_mesh)
 
-    camera_positions, focal_points = generate_camera_positions_along_wall(num_views, radius, height, wall_distance)
 
-    camera_data = render_images_bg(pv_mesh, texture, camera_positions, focal_points, intrinsics, output_dir)
+   fx, fy = 800, 800
+   cx, cy = 640, 480
+   intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
-    os.makedirs(os.path.dirname(output_json), exist_ok=True)
-    save_camera_data(camera_data, output_json)
+
+   num_views = 125
+   radius = 2
+   camera_positions, focal_points = generate_interior_camera_positions(num_views, radius)
+
+
+   camera_data = render_interior_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, focal_points)
+
+
+   os.makedirs(os.path.dirname(output_json), exist_ok=True)
+   save_camera_data(camera_data, output_json)
 
 
 def generate_object():
-    file_path = 'data/ficus_bonsai.glb'
-    scene_name = "object"
-    output_dir = f'{scene_name}/images'
-    output_json = f'{scene_name}/output/cameras.json'
+   file_path = 'data/ficus_bonsai.glb'
+   scene_name = "object"
+   output_dir = f'{scene_name}/images'
+   output_json = f'{scene_name}/output/cameras.json'
 
-    scene = load_model(file_path)
-    if not scene:
-        return
 
-    mesh = merge_geometries(scene)
+   scene = load_model(file_path)
+   if not scene:
+       return
 
-    pv_mesh = convert_to_pyvista(mesh)
-    if not pv_mesh:
-        return
 
-    texture = handle_texture(mesh, pv_mesh)
+   mesh = merge_geometries(scene)
 
-    fx, fy = 800, 800
-    cx, cy = 640, 480
-    intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
-    num_views = 125
-    radius = 10
-    camera_positions = generate_camera_positions(num_views, radius)
+   pv_mesh = convert_to_pyvista(mesh)
+   if not pv_mesh:
+       return
 
-    camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
 
-    os.makedirs(os.path.dirname(output_json), exist_ok=True)
-    save_camera_data(camera_data, output_json)
+   texture = handle_texture(mesh, pv_mesh)
+
+
+   fx, fy = 800, 800
+   cx, cy = 640, 480
+   intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
+
+
+   num_views = 125
+   radius = 10
+   camera_positions = generate_camera_positions(num_views, radius)
+
+
+   camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
+
+
+   os.makedirs(os.path.dirname(output_json), exist_ok=True)
+   save_camera_data(camera_data, output_json)
+
 
 if __name__ == "__main__":
-    generate_background()
-    generate_object()
+   generate_background()
+   generate_object()
