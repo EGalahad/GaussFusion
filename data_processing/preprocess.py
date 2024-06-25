2cadda54f203f7c948b27f17f1c5635d3c408801
jasmine27-alt <jaswong548@gmail.com>
2024-06-25 19:16:04 +0800

    Update preprocess.py: Pyvista Implementation that Works with Textures
    Former-commit-id: ab3541c69ce1902f23b8dbdaa64acee0a85d8402
    Former-commit-id: 0d02ccc9299d9967ef51f9cbc9755f0d38809cd8 [formerly 2194113c972ee1b744c47d2836b89e6293ea2721]
    Former-commit-id: 02738f87ed41be6d1289815e638fc1c0a24cee50

--- data_processing/preprocess.py
+++ data_processing/preprocess.py
@@ -1,95 +1,32 @@
 from utils import *
 import os
-
-
-def generate_background():
-   file_path = 'data/cozy_modern_bedroom.glb'
-   scene_name = "background"
-   output_dir = f'{scene_name}/images'
-   output_json = f'{scene_name}/output/cameras.json'
-
-
-   scene = load_model(file_path)
-   if not scene:
-       return
-
-
-   mesh = merge_geometries(scene)
-   if not mesh:
-       return
-
-
-   # Rotate the mesh 90 degrees around the axis [1, 0, 1]
-   #axis = np.array([1, 0, 1])
-   #angle = np.pi / 2  # 90 degrees in radians
-   #mesh = apply_rotation(mesh, axis, angle)
-
-
-   pv_mesh = convert_to_pyvista(mesh)
-   if not pv_mesh:
-       return
-
-
-   texture = handle_texture(mesh, pv_mesh)
-
-
-   fx, fy = 800, 800
-   cx, cy = 640, 480
-   intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
-
-
-   num_views = 125
-   radius = 2
-   camera_positions, focal_points = generate_interior_camera_positions(num_views, radius)
-
-
-   camera_data = render_interior_images(pv_mesh, texture, camera_positions, intrinsics, output_dir, focal_points)
-
-
-   os.makedirs(os.path.dirname(output_json), exist_ok=True)
-   save_camera_data(camera_data, output_json)
-
+import pyvista as pv
 
 def generate_object():
-   file_path = 'data/ficus_bonsai.glb'
-   scene_name = "object"
-   output_dir = f'{scene_name}/images'
-   output_json = f'{scene_name}/output/cameras.json'
+    file_path = 'data/scene.gltf'
+    scene_name = "object"
+    output_dir = f'{scene_name}/images'
+    output_json = f'{scene_name}/output/cameras.json'
 
+    # Set up the plotter
+    pl = pv.Plotter(off_screen=True)
 
-   scene = load_model(file_path)
-   if not scene:
-       return
+    # Import the bonsai GLTF file
+    pl.import_gltf(file_path)
 
+    fx, fy = 800, 800
+    cx, cy = 640, 480
+    intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
 
-   mesh = merge_geometries(scene)
+    num_views = 150
+    radius = 2
+    camera_positions = generate_camera_positions(num_views, radius)
 
+    camera_data = render_images(pl, camera_positions, intrinsics, output_dir)
 
-   pv_mesh = convert_to_pyvista(mesh)
-   if not pv_mesh:
-       return
+    os.makedirs(os.path.dirname(output_json), exist_ok=True)
 
-
-   texture = handle_texture(mesh, pv_mesh)
-
-
-   fx, fy = 800, 800
-   cx, cy = 640, 480
-   intrinsics = generate_camera_intrinsics(fx, fy, cx, cy)
-
-
-   num_views = 125
-   radius = 10
-   camera_positions = generate_camera_positions(num_views, radius)
-
-
-   camera_data = render_images(pv_mesh, texture, camera_positions, intrinsics, output_dir)
-
-
-   os.makedirs(os.path.dirname(output_json), exist_ok=True)
-   save_camera_data(camera_data, output_json)
-
+    save_camera_data(camera_data, output_json)
 
 if __name__ == "__main__":
-   generate_background()
-   generate_object()
+    generate_object()
